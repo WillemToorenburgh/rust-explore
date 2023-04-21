5eb1ffd68925a3b2df72045fd7f9c2e35bec5666
Willem Toorenburgh <willemtoorenburgh@gmail.com>
2023-04-21 16:32:36 -0700

    Error handling and conversion of images to mut vec

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,29 @@ use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let res = reqwest::get("https://api.thecatapi.com/v1/images/search")
-        .await
-        .unwrap();
+    let url = get_cat_image_url().await.unwrap();
+    println!("URL: {}", url)
+}
+
+async fn get_cat_image_url() -> color_eyre::Result<String> {
+    let api_url = "https://api.thecatapi.com/v1/images/search";
+    let res = reqwest::get(api_url).await?;
     if !res.status().is_success() {
-        panic!("Request failed with HTTP {}", res.status());
+        return Err(color_eyre::eyre::eyre!(
+            "Request failed with HTTP {}",
+            res.status()
+        ));
     }
 
     #[derive(Deserialize)]
     struct CatImage {
-        url: String
+        url: String,
     }
 
-    let images: Vec<CatImage> = res.json().await.unwrap();
-    let image = images
-        .first()
-        .expect("The cat API should return at least one image");
-    println!("URL: {}", image.url)
+    let mut images: Vec<CatImage> = res.json().await?;
+    let Some(image) = images.pop() else {
+        return Err(color_eyre::eyre::eyre!("The cat API did not return an image"));
+    };
+
+    Ok(image.url)
 }
