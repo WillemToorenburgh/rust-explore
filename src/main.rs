e4e22ce23a0139b5d3b598ac42cc4ad956e14d6a
Willem Toorenburgh <willemtoorenburgh@gmail.com>
2023-04-21 17:27:58 -0700

    Correctly deliver HTML to clients

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,41 @@
-// use pretty_hex::PrettyHex;
+use reqwest::StatusCode;
 use serde::Deserialize;
+
+use axum::{
+    body::BoxBody,
+    http::header,
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
 
 #[tokio::main]
 async fn main() {
+    let app = Router::new().route("/", get(root_get));
+
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+
     let art = get_cat_ascii_art().await.unwrap();
     println!("{art}");
+}
+
+async fn root_get() -> Response {
+    match get_cat_ascii_art().await {
+        Ok(art) => (
+            StatusCode::OK,
+            [(header::CONTENT_TYPE, "text/html; charset=utf-8")],
+            art,
+        )
+            .into_response(),
+
+        Err(e) => {
+            println!("Encountered error: {e}");
+            (StatusCode::INTERNAL_SERVER_ERROR, "Encountered error").into_response()
+        }
+    }
 }
 
 async fn get_cat_ascii_art() -> color_eyre::Result<String> {
@@ -36,7 +67,12 @@ async fn get_cat_ascii_art() -> color_eyre::Result<String> {
         .to_vec();
 
     let image = image::load_from_memory(&image_bytes)?;
-    let ascii_art = artem::convert(image, artem::options::OptionBuilder::new().build());
+    let ascii_art = artem::convert(
+        image,
+        artem::options::OptionBuilder::new()
+            .target(artem::options::TargetType::HtmlFile(true, true))
+            .build(),
+    );
 
     Ok(ascii_art)
 }
